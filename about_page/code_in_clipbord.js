5d702bd6fcb5e7ed3f00e74b472ce47b7229e256
White-On <74248238+White-On@users.noreply.github.com>
2023-08-10 18:18:26 +0200

    updating the about pages

--- about_page/code_in_clipbord.js
+++ about_page/code_in_clipbord.js
@@ -60,3 +60,58 @@ for (let i = 0; i < codeElements.length; i++) {
 }
 
 
+// const seperate = id => {
+//     const element = document.getElementById(id),
+//           text = element.innerText.split("");
+//     element.innerText = "";
+//     text.forEach(letter =>{
+//       const span = document.createElement("span");
+//       span.className = "letter";
+//       span.innerText = letter;
+//       element.appendChild(span);
+//     });
+// }
+
+// seperate("main-title");
+
+const caracters = "ABCDEFGHIJKMNLOPQRSTUVWXYZ";
+
+const element = document.getElementById("main-title");
+element.onmouseover = (event) => {
+    glitch("main-title");
+};
+
+const glitch = id => {
+    const glitchElement = document.getElementById(id);
+    let iterations = -5;
+    // a list going to 0 to the length of the text
+    const letterToFix = glitchElement.innerText.split("").map((letter, index) => index);
+    const letterOrderToFix = letterToFix.sort(() => Math.random() - 0.5);
+    const interval = setInterval(() => {
+      glitchElement.innerText = glitchElement.innerText
+        .split("")
+        .map((letter, index) => {
+            // create the list of the n first letters to fix
+            let letterIndex = [];
+            if (iterations > 0) {
+                letterIndex =  letterOrderToFix.slice(0, iterations);
+            }
+            
+            if (letterIndex.includes(index)) {
+                return glitchElement.dataset.value[index];
+            }
+            return caracters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+      if (iterations >= glitchElement.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iterations += 1 / 3;
+    }, 30);
+}
+
+// every 20 seconds we glitch the title
+setInterval(function () {
+    glitch("main-title");
+}, 20000);
